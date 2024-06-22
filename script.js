2624ba89f219a5fb866f3235add2d3b5a950df47
Alfonsoce11 <alfonsocespino11@gmail.com>
2024-06-21 23:38:10 -0700

    modified:   script.js

--- script.js
+++ script.js
@@ -119,21 +119,18 @@ document
     console.log(data);
   });
 
-  document.getElementById('openPopup').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'block';
-  });
+document.getElementById("openPopup").addEventListener("click", function () {
+  document.getElementById("popup").style.display = "block";
+});
 
-  document.getElementById('closePopup').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'none';
-  });
+document.getElementById("closePopup").addEventListener("click", function () {
+  document.getElementById("popup").style.display = "none";
+});
 
-  document.getElementById('openPopup2').addEventListener('click', function() {
-    document.getElementById('popup2').style.display = 'block';
-  });
+document.getElementById("openPopup2").addEventListener("click", function () {
+  document.getElementById("popup2").style.display = "block";
+});
 
-  document.getElementById('closePopup2').addEventListener('click', function() {
-    document.getElementById('popup2').style.display = 'none';
-  });
-
-const newURL = 'Alfonso.com'`;
-window.history.pushState({ path: newURL }, '', newURL);
+document.getElementById("closePopup2").addEventListener("click", function () {
+  document.getElementById("popup2").style.display = "none";
+});
