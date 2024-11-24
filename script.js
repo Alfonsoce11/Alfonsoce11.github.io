63a626e485928769ff4ff7216eeeb8f9dddff5c4
Alfonso <alfonsocespino10@gmail.com>
2024-11-24 22:56:58 +0000

    Remove toggle for openButton class on sidebar close and add transition styling for menu button

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ document.addEventListener("click", function (event) {
   if (!sidebar.contains(event.target) && event.target !== toggleButton) {
     sidebar.classList.remove("open");
     document.getElementById("menu-button").classList.remove("sidebar-toggle-close")
-    document.getElementById("menu-button").classList.toggle("openButton");
     )
   }
 });
